9c0c0a0f5de84cf9c67791808ba771102e2a70aa
Nguyễn Hồng Phúc <phucdep1237@gmail.com>
2026-04-13 09:38:25 +0700

    Time: 0 ms (100%), Space: 17 MB (67.18%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double findMaxAverage(int* nums, int n, int k) {
+    int sum = 0;
+    for(int i = 0; i<k; i++)
+    {
+        sum += nums[i];
+    }
+
+    int max_sum = sum;
+    for(int i = k; i < n; i++)
+    {
+        sum = sum - nums[i-k] + nums[i];
+        if(sum > max_sum)
+        {
+            max_sum = sum;
+        }
+
+    }
+    return (double)max_sum / k;
+
+}
