db014afb8844c11ea241c3cf36f6e2507fa8baa2
Nguyễn Hồng Phúc <phucdep1237@gmail.com>
2026-04-13 09:37:03 +0700

    Time: 0 ms (100%), Space: 8.6 MB (66.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isValid(char* s)
+{
+    int len = strlen(s);
+    char stack[len];
+    int top = -1;
+
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        char c = s[i];
+
+        if(c == '(' || c == '[' || c == '{')
+        {
+            stack[++top] = c;
+        }
+        else
+        {
+            if(top == -1) return false;
+
+            char t = stack[top--];
+
+            if((c == ')' && t != '(') ||
+               (c == ']' && t != '[') ||
+               (c == '}' && t != '{'))
+                return false;
+        }
+    }
+
+    return top == -1;
+}
