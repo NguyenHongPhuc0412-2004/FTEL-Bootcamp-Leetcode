1894d740ba13520220c30d9975ca65b794bec2cb
Nguyễn Hồng Phúc <phucdep1237@gmail.com>
2026-04-13 09:38:59 +0700

    Time: 0 ms (100%), Space: 9.6 MB (28.06%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,16 @@
+bool canConstruct(char * ransomNote, char * magazine){
+    int freq[26] = {0};
+
+    for (int i = 0; magazine[i] != '\0'; i++) {
+        freq[magazine[i] - 'a']++;
+    }
+    
+    for (int i = 0; ransomNote[i] != '\0'; i++) {
+        freq[ransomNote[i] - 'a']--;
+        if (freq[ransomNote[i] - 'a'] < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
